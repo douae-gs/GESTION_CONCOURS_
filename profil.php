aa27c03b0f29b8b1df70727d84fc34de286a1adf
douae-gs <douae.gs25@gmail.com>
2026-06-03 23:33:59 +0100

    petite modification

--- profil.php
+++ profil.php
@@ -97,6 +97,7 @@ $nb_candidatures = $stmt2->fetchColumn();
     </div>
 </div>
 
+    
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="js/main.js"></script>
 </body>
